66221518e44836fe60e38182eb406117e63fcf58
ruixi <243250476@qq.com>
2024-06-24 17:37:55 +0800

    feat: enhance tRPC context to get session and create logger middleware

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,11 +1,34 @@
-import { initTRPC } from '@trpc/server';
+import { initTRPC, TRPCError } from '@trpc/server';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/server/auth';
 
-const t = initTRPC.create();
+export const createTRPCContext = async () => {
+    const session = await getServerSession(authOptions);
+    if (!session?.user) {
+        throw new TRPCError({
+            code: 'FORBIDDEN',
+        });
+    }
+    return session;
+};
+
+const t = initTRPC.context<typeof createTRPCContext>().create();
 
 const { router, procedure } = t;
+const middleware = t.middleware(async ({ ctx, next }) => {
+    const start = Date.now();
+    const result = await next();
+
+    console.log('Api operation time: ', Date.now() - start);
+
+    return result;
+});
+
+const loggerProcedure = procedure.use(middleware);
 
 export const testRouter = router({
-    hello: procedure.query(() => {
+    hello: loggerProcedure.query(async ({ ctx }) => {
+        console.log('------------>', ctx.user);
         return {
             helle: 'up',
         };
